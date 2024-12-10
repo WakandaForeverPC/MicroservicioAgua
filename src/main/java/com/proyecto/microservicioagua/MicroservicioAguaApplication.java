f4438db138a0463b076e2bce849dc09097cb740d
Samuu10 <svallmun@myuax.com>
2024-12-10 18:28:28 +0100

    Initial commit

---
+++ src/main/java/com/proyecto/microservicioagua/MicroservicioAguaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.microservicioagua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioAguaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioAguaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyecto/microservicioagua/MicroservicioAguaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.microservicioagua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioAguaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
